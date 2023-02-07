773e9f12c21692628ebfbfff64d9e6c4493cd3e3
Felix <felix.roessel@elastic.co>
2023-02-07 14:52:38 +0100

    Added mapping from AWS to Elastic regions

--- terraform/terraform/variables.tf
+++ terraform/terraform/variables.tf
@@ -8,7 +8,7 @@ variable "elastic_version" {
 
 variable "elastic_region" {
   type = string
-  default = "aws-eu-west-2"
+  default = "eu-west-2"
 }
 
 variable "elastic_deployment_name" {
@@ -41,4 +41,29 @@ variable "aws_secret_key" {
 variable "bucket_name" {
   type = string
   default = "elastic-sar-bucket"
+}
+
+variable "mapped_elastic_region" {
+  type = map(string)
+  default = {
+    "af-south-1" = "aws-af-south-1"
+    "ap-east-1" = "aws-ap-east-1"
+    "ap-northeast-1" = "ap-northeast-1"
+    "ap-northeast-2" = "aws-ap-northeast-2"
+    "ap-south-1" =     "aws-ap-south-1"
+    "ap-southeast-1" = "ap-southeast-1"
+    "ap-southeast-2" = "ap-southeast-2"
+    "eu-central-1" = "aws-eu-central-1"
+    "eu-south-1" =     "aws-eu-south-1"
+    "eu-west-1" =     "eu-west-1"
+    "eu-west-2" =     "aws-eu-west-2"
+    "eu-west-3" =     "aws-eu-west-3"
+    "me-south-1" =     "aws-me-south-1"
+    "sa-east-1" =     "sa-east-1"
+    "us-east-1" =     "us-east-1"
+    "us-east-2" =     "aws-us-east-2"
+    "us-west-1" =     "us-west-1"
+    "us-west-2" =     "us-west-2"
+    "ca-central-1" = "aws-ca-central-1"
+  }
 }
